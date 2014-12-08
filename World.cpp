c421a50505ec0865230e02d22bebc7da3d7c7fab
luke <luke@DsLaptop>
2014-12-08 00:11:11 -0500

    -added virtual to destructors, -changed player initialization to conform to RAII -added "shell" Map classes, trying to build something to conform to MVC, but will need more thought

---
+++ World.cpp
@@ -0,0 +1,35 @@
+#include "World.hpp"
+#include "Biome.hpp"
+#include "Tile.hpp"
+#include <vector>
+#include <iostream>
+
+World::World(int xSize, int ySize) : baseMap(xSize, std::vector<Biome>(ySize)) {
+}
+
+World::~World() {
+}
+
+
+Tile World::getTile(int xPos, int yPos)
+{
+    int displayId = baseMap[xPos][yPos].getDisplayID();
+    
+    return Tile(displayId, al_map_rgb(20,30,40), al_map_rgb(40,30,20));
+    
+}
+
+void World::buildBiomes()
+{
+    TerrainGenerator tGen;
+    
+    for(int x = 0; x < baseMap.size(); x++)
+    {
+        for(int y = 0; y < baseMap[x].size(); y++)
+        {
+            baseMap[x][y].setDisplayID(tGen.generateRandomTerrain());
+        }
+            
+    }
+}
+
